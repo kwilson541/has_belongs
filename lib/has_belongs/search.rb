999dfb16a10ef8c81001c4e4834eb8dd716eac04
James <jamesdix@cwgsy.net>
2017-01-19 14:48:00 +0000

    habtm finished and successful

--- lib/has_belongs/search.rb
+++ lib/has_belongs/search.rb
@@ -4,6 +4,14 @@ require 'active_support/inflector'
 module HasBelongs
 	class Search
 
+		def initialize
+			@parent_model = ""
+			@child_models = []
+			@has_and_belongs_child_models = []
+			@migrations = []
+		end
+
+
 		def return_files(filepath = "app/models")
 			if Dir.exist?(filepath)
 				search_path = filepath + "/*.rb"
@@ -35,27 +43,15 @@ module HasBelongs
 
 		def generate_migration(filepath = "app/models")
 			files = return_has_many(filepath)
-			@parent_model = ""
-			@child_models = []
-			@has_and_belongs_child_models = []
-			migrations = []
 			files.each do |file|
-				File.open(file).each_line { |line|
+				File.open(file).each_line do |line|
 					class_has(line)
 					class_belongs(line)
-				}
-				@child_models.each do |model|
-					relationship = "Add#{@parent_model}RefTo#{model.capitalize}"
-					if !relationship_exist?(relationship)
-						migrations << "bin/rails g migration #{relationship} #{@parent_model.downcase}:references"
-					end
-				@child_models = []
 				end
-				@has_and_belongs_child_models.each do |model|
-					relationship = "CreateJoinTable "
-			@parent_model = ""
+				add_one_and_many_migrations
+				add_habtm_migrations
 			end
-			migrations
+			@migrations
 		end
 
 		def relationship_exist?(relationship, filepath = "db/migrate/*.rb")
@@ -68,6 +64,27 @@ module HasBelongs
 
 
 		private
+
+		def add_one_and_many_migrations
+			@child_models.each do |model|
+				relationship = "Add#{@parent_model}RefTo#{model.capitalize}"
+				if !relationship_exist?(relationship)
+					@migrations << "bin/rails g migration #{relationship} #{@parent_model.downcase}:references"
+				end
+				@child_models = []
+			end
+		end
+
+		def add_habtm_migrations
+			@has_and_belongs_child_models.each do |model|
+				relationship_A = "create_join_table :#{@parent_model}, :#{model}"
+				relationship_B = "create_join_table :#{model}, :#{@parent_model}"
+				if !relationship_exist?(relationship_A) && !relationship_exist?(relationship_B)
+					@migrations << "bin/rails g migration CreateJoinTable #{@parent_model.downcase} #{model}"
+				end
+				@parent_model = ""
+			end
+		end
 
 		def class_has(line)
 			if line =~ /(class).*( < ApplicationRecord)/

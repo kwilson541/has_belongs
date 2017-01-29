fbdcdf776d796abd8ee63816a734f4bb996bf558
Kim Wilson <k.wilson541@gmail.com>
2017-01-29 11:25:49 +0000

    Moved all test model folders into one folder

--- spec/add_spec.rb
+++ spec/add_spec.rb
@@ -12,31 +12,31 @@ describe 'add' do
 	end
 
 	it 'raises an error when it cannot find any files in the directory' do
-		expect{add.return_files("spec/test_models_empty")}.to raise_error("Could not find any files in spec/test_models_empty")
+		expect{add.return_files("spec/test_models/empty")}.to raise_error("Could not find any files in spec/test_models/empty")
 	end
 
 	it 'raises an error if appropriate files do not contains "has_many"' do
-		expect{add.return_has_many("spec/test_models_without_associations")}.to raise_error("No associations found")
+		expect{add.return_has_many("spec/test_models/without_associations")}.to raise_error("No associations found")
 	end
 
 	it 'returns an array of all ruby files in app/models' do
-		expect(add.return_files("spec/test_models")).to eq ["spec/test_models/bacon.rb", "spec/test_models/piggy.rb"]
+		expect(add.return_files("spec/test_models/one_to_many")).to eq ["spec/test_models/one_to_many/bacon.rb", "spec/test_models/one_to_many/piggy.rb"]
 	end
 
 	it 'returns an array of all ruby files containing "has_many" keyword' do
-		expect(add.return_has_many("spec/test_models")).to eq ["spec/test_models/piggy.rb"]
+		expect(add.return_has_many("spec/test_models/one_to_many")).to eq ["spec/test_models/one_to_many/piggy.rb"]
 	end
 
 	it 'returns an array of all ruby files containing "has_one" keyword' do
-		expect(add.return_has_many("spec/test_models_has_one")).to eq ["spec/test_models_has_one/piggy3.rb"]
+		expect(add.return_has_many("spec/test_models/has_one")).to eq ["spec/test_models/has_one/piggy3.rb"]
 	end
 
 	it 'returns the Rails generate method for files with "has_many" keyword' do
-		expect(add.generate_migration("spec/test_models")).to eq ["bin/rails g migration AddPiggyRefToBacon piggy:references"]
+		expect(add.generate_migration("spec/test_models/one_to_many")).to eq ["bin/rails g migration AddPiggyRefToBacon piggy:references"]
 	end
 
 	it 'returns the Rails generate method for files with "has_one" keyword' do
-		expect(add.generate_migration("spec/test_models_has_one")).to eq ["bin/rails g migration AddPiggyRefToTail piggy:references"]
+		expect(add.generate_migration("spec/test_models/has_one")).to eq ["bin/rails g migration AddPiggyRefToTail piggy:references"]
 	end
 
 	it 'returns true if migration exists' do
@@ -48,23 +48,23 @@ describe 'add' do
 	end
 
 	it 'returns false if migration does not exist' do
-		expect(add.relationship_exist?("AddUserRefToComments", "*/test_models_empty/*.rb")).to be false
+		expect(add.relationship_exist?("AddUserRefToComments", "*/test_models/empty/*.rb")).to be false
 	end
 
 	it 'returns multiple generate methods for a single file with multiple "has_many" keywords' do
-		expect(add.generate_migration("spec/test_models_multiple_associations")).to eq ["bin/rails g migration AddPiggyRefToBacon piggy:references", "bin/rails g migration AddPiggyRefToHam piggy:references"]
+		expect(add.generate_migration("spec/test_models/multiple_associations")).to eq ["bin/rails g migration AddPiggyRefToBacon piggy:references", "bin/rails g migration AddPiggyRefToHam piggy:references"]
 	end
 
 	it 'returns a file with "has_and_belongs_to_many" keyword' do
-		expect(add.return_has_many("spec/test_models_habtm")).to eq ["spec/test_models_habtm/bacon.rb"]
+		expect(add.return_has_many("spec/test_models/habtm")).to eq ["spec/test_models/habtm/bacon.rb"]
 	end
 
 	it 'returns migration method for a join table' do
-		expect(add.generate_migration("spec/test_models_habtm")).to eq ["bin/rails g migration CreateJoinTable bacon piggy"]
+		expect(add.generate_migration("spec/test_models/habtm")).to eq ["bin/rails g migration CreateJoinTable bacon piggy"]
 	end
 
 	it 'returns only one migration method for each join table' do
-		expect(add.generate_migration("spec/test_models_multiple_habtm")).to eq ["bin/rails g migration CreateJoinTable bacon piggy"]
+		expect(add.generate_migration("spec/test_models/multiple_habtm")).to eq ["bin/rails g migration CreateJoinTable bacon piggy"]
 	end
 
 end
--- spec/remove_spec.rb
+++ spec/remove_spec.rb
@@ -13,23 +13,23 @@ describe 'remove_assocations' do
   end
 
   it 'finds the related model file' do
-    expect(remove.set_parent_file('spec/test_models/', "spec/db_test/schema.rb")).to eq [["spec/test_models/author.rb", "books"], ["spec/test_models/book.rb", "pages"]]
+    expect(remove.set_parent_file('spec/test_models/with_associations/', "spec/db_test/schema.rb")).to eq [["spec/test_models/with_associations/author.rb", "books"], ["spec/test_models/with_associations/book.rb", "pages"]]
   end
 
   it 'checks if file no longer contains a "has_many" relationship' do
-    expect(remove.non_existing_relationships('spec/test_models_with_and_without_associations/', "spec/db_test/schema.rb")).to eq [['spec/test_models_with_and_without_associations/author.rb', "books"]]
+    expect(remove.non_existing_relationships('spec/test_models/with_and_without_associations/', "spec/db_test/schema.rb")).to eq [['spec/test_models/with_and_without_associations/author.rb', "books"]]
   end
 
   it 'checks if file no longer contains multiple associations' do
-    expect(remove.non_existing_relationships('spec/test_models_remove_many_and_one/', "spec/db_test/schema2.rb")).to eq [['spec/test_models_remove_many_and_one/author.rb', "books"], ['spec/test_models_remove_many_and_one/author.rb', "pages"]]
+    expect(remove.non_existing_relationships('spec/test_models/remove_many_and_one/', "spec/db_test/schema2.rb")).to eq [['spec/test_models/remove_many_and_one/author.rb', "books"], ['spec/test_models/remove_many_and_one/author.rb', "pages"]]
   end
 
   it 'should raise an error if unmigration is not needed' do
-    expect{remove.remove_migrations('spec/test_models_with_associations/', "spec/db_test/schema.rb")}.to raise_error("All files still contain relationships")
+    expect{remove.remove_migrations('spec/test_models/with_associations/', "spec/db_test/schema.rb")}.to raise_error("All files still contain relationships")
   end
 
   it "will generate the remove commands" do
-    expect(remove.generate_has_and_one_remove_migrations('spec/test_models_with_and_without_associations/', "spec/db_test/schema.rb")).to eq(["bin/rails g migration RemoveAuthorRefFromBooks author:references"])
+    expect(remove.generate_has_and_one_remove_migrations('spec/test_models/with_and_without_associations/', "spec/db_test/schema.rb")).to eq(["bin/rails g migration RemoveAuthorRefFromBooks author:references"])
   end
 
   pending it 'can find join table models' do
@@ -37,7 +37,7 @@ describe 'remove_assocations' do
   end
 
   pending it 'can generate remove commands for removing habtm association' do
-    expect(remove.generate_habtm_remove_migrations("spec/test_models_remove_habtm/", "spec/db_test/schema.rb")).to eq ["bin/rails g migration RemoveJoinTable book page"]
+    expect(remove.generate_habtm_remove_migrations("spec/test_models/remove_habtm/", "spec/db_test/schema.rb")).to eq ["bin/rails g migration RemoveJoinTable book page"]
   end
 
 end
